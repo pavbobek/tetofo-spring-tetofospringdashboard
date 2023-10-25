df6a2af1366be4fbc0549fa6df31426b31597b2b
Pavel <pavbobek@seznam.cz>
2023-10-25 10:04:25 +0200

    enum as number serialization

--- src/main/java/tetofo/spring/tetofospringdashboard/Model/Enum/Tag.java
+++ src/main/java/tetofo/spring/tetofospringdashboard/Model/Enum/Tag.java
@@ -1,8 +1,15 @@
 package tetofo.spring.tetofospringdashboard.Model.Enum;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Tag {
     DIRECTORY_PATH,
     FILENAME,
     MESSAGE,
-    STRING
+    STRING;
+
+    @JsonValue
+    public int toValue() {
+        return ordinal();
+    }
 }
