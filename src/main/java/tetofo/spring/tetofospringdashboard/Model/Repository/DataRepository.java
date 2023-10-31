6dbf4f5b84b6a60940bd0376645808bdb3652df0
Pavel <pavbobek@seznam.cz>
2023-10-31 11:33:39 +0100

    added repository

---
+++ src/main/java/tetofo/spring/tetofospringdashboard/Model/Repository/DataRepository.java
@@ -0,0 +1,9 @@
+package tetofo.spring.tetofospringdashboard.Model.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import tetofo.spring.tetofospringdashboard.Model.Entity.DataEntity;
+
+@Repository
+public interface DataRepository extends CrudRepository<DataEntity, Long> {}
