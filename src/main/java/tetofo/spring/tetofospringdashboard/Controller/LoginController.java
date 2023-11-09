22f230dca6b75a2492831b6b01f227ddbcf263e2
Pavel <pavbobek@seznam.cz>
2023-11-09 11:31:53 +0100

    removed Thymeleaf

--- src/main/java/tetofo/spring/tetofospringdashboard/Controller/LoginController.java
+++ src/main/java/tetofo/spring/tetofospringdashboard/Controller/LoginController.java
@@ -1,11 +1,11 @@
 package tetofo.spring.tetofospringdashboard.Controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/auth")
 public class LoginController {
     @GetMapping("/login")
